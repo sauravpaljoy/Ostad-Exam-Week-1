7f708f1b57a8f795f01dfa65798defeaabfff59b
sauravpaljoy <77772861+sauravpaljoy@users.noreply.github.com>
2024-04-13 21:09:50 +0600

    Update bank.js

--- Bank_app/bank.js
+++ Bank_app/bank.js
@@ -7,7 +7,7 @@ class BankAccount {
 
     deposit(amount) {
         this.balance += amount;
-        console.log(`Deposited $${amount} into account : $${this.accountNumber}  New balance: $${this.balance}`);
+        console.log(`Deposited ৳${amount} into account : ৳${this.accountNumber}  New balance: ৳${this.balance}`);
     }
 
     withdraw(amount) {
@@ -15,7 +15,7 @@ class BankAccount {
             console.log("Insufficient funds");
         } else {
             this.balance -= amount;
-            console.log(`Withdrawn $${amount} From account : $${this.accountNumber} New balance: $${this.balance}`);
+            console.log(`Withdrawn ৳${amount} From account : ৳${this.accountNumber} New balance: ৳${this.balance}`);
         }
     }
 
@@ -26,25 +26,25 @@ class BankAccount {
     displayAccountInfo() {
         console.log(`Account Number: ${this.accountNumber}`);
         console.log(`Owner Name: ${this.ownerName}`);
-        console.log(`Balance: $${this.balance}`);
+        console.log(`Balance: ৳${this.balance}`);
     }
     displayupdateInfo() {
         console.log(`Account Number: ${this.accountNumber}`);
         console.log(`Owner Name: ${this.ownerName}`);
-        console.log(`Balance: $${this.balance}`);
+        console.log(`Balance: ৳${this.balance}`);
     }
 
 }
 
 // Creating two instances of BankAccount
-const account = new BankAccount(1001, "John Doe", 500);
+const account = new BankAccount(2024000121, "Saurav pal", 50000);
 
 // Demonstrating functionality
 console.log("\nInitial Account Information:");
 account.displayAccountInfo(); 
 console.log("\nDeposit And Withdrawal:");
-account.deposit(200);
-account.withdraw(100);
+account.deposit(2000);
+account.withdraw(10000);
 console.log("\nUpdated Account Information:");
 account.displayupdateInfo();
 
